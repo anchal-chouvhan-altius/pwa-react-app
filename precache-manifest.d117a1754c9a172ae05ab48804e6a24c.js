abda96ffcda0bb346b6deafae9b89df3bdf14a32
Anchal Chauhan <anchal.c@altius.cc>
2019-12-24 14:13:03 +0530

    Updates

---
+++ precache-manifest.d117a1754c9a172ae05ab48804e6a24c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4f0301e16264fee0c2383bcdb9be37",
+    "url": "/pwa-react-app/index.html"
+  },
+  {
+    "revision": "cd6027b323280e1fdf4a",
+    "url": "/pwa-react-app/static/css/main.2a3f1e63.chunk.css"
+  },
+  {
+    "revision": "db54ed6b935544dcf89b",
+    "url": "/pwa-react-app/static/js/2.9eb1b836.chunk.js"
+  },
+  {
+    "revision": "cd6027b323280e1fdf4a",
+    "url": "/pwa-react-app/static/js/main.abe47692.chunk.js"
+  },
+  {
+    "revision": "b8eded8142d07a01202c",
+    "url": "/pwa-react-app/static/js/runtime-main.b4640bf3.js"
+  }
+]);
