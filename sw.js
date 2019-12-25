d28fabf9fdde49b7234738c204b142ba00cc742a
Anchal Chauhan <anchal.c@altius.cc>
2019-12-25 15:27:03 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-var cacheName = 'todo-app';
+var version = 'todo-app_v1';
+var cacheName = version;
 var assets = [
     '/static/js/bundle.js',
     '/static/js/bundle.js.map',
@@ -16,7 +17,7 @@ var assets = [
     '/static/media/logo.5d5d9eef.svg',
     'favicon.ico'
     // '/'
-  ];
+];
 
 self.addEventListener('install', event => {
     console.log('service worker installed');
@@ -27,46 +28,69 @@ self.addEventListener('install', event => {
         }).then(() => self.skipWaiting())
     )
 });
-self.addEventListener('activate',function(event){
+self.addEventListener('activate', function (event) {
     console.log('service worker activated');
     event.waitUntil(
         caches.keys().then(keys => {
             //console.log(keys);
             return Promise.all(keys
-              .filter(key => key !== cacheName)
-              .map(key => caches.delete(key))
+                .filter(key => key !== cacheName)
+                .map(key => caches.delete(key))
             );
-          })
-    );  
+        })
+    );
 })
 
 self.addEventListener('fetch', evt => {
     console.log('fetch event', evt);
+    const url = new URL(evt.request.url);
+    console.log("url.origin---"+url.origin);
+    console.log("location.origin---"+location.origin);
+    console.log("url.pathname---"+url.pathname);
+
     evt.respondWith(
-      caches.match(evt.request).then(cacheRes => {
-        return cacheRes || fetch(evt.request);
-      })
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
     );
-  });
-// activate event
-// self.addEventListener('activate', evt => {
-//     console.log('service worker activated');
-//     evt.waitUntil(
-//       caches.keys().then(keys => {
-//         //console.log(keys);
-//         return Promise.all(keys
-//           .filter(key => key !== cacheName)
-//           .map(key => caches.delete(key))
-//         );
-//       })
-//     );
-//   });
-// fetch event
-// self.addEventListener('fetch', evt => {
-//   console.log('fetch event', evt);
-//   evt.respondWith(
-//     caches.match(evt.request).then(cacheRes => {
-//       return cacheRes || fetch(evt.request);
-//     })
-//   );
+});
+self.addEventListener('message', (event) => {
+    console.log('[Service Worker] Message Event: ', event.data)
+});
+
+/**
+ * Listen for incoming Push events
+ */
+// addEventListener('push', (event) => {
+// 	console.log('[Service Worker] Push Received.');
+// 	console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+// 	if (!(self.Notification && self.Notification.permission === 'granted'))
+// 		return;
+
+// 	var data = {};
+//   if (event.data)
+//     data = event.data.json();
+
+// 	var title = data.title || "Web Push Notification";
+// 	var message = data.message || "New Push Notification Received";
+// 	var icon = "images/notification-icon.png";
+// 	var badge = 'images/notification-badge.png';
+// 	var options = {
+// 		body: message,
+// 		icon: icon,
+// 		badge: badge
+// 	};
+// 	event.waitUntil(self.registration.showNotification(title,options));
+// });
+
+// /**
+//  * Handle a notification click
+//  */
+// addEventListener('notificationclick', (event) => {
+// 	console.log('[Service Worker] Notification click: ', event);
+// 	event.notification.close();
+// 	event.waitUntil(
+// 		clients.openWindow('https://aws-amplify.github.io/amplify-js')
+// 	);
 // });
