e3ff81d3acb65ce26dffea35e3d01b41878d47bf
Anchal Chauhan <anchal.c@altius.cc>
2019-12-26 16:29:30 +0530

    Updates

--- pipelinesw.js
+++ pipelinesw.js
@@ -1,8 +1,9 @@
-var cacheName = 'todo-app_v2';
+var cacheName = 'todo-app_v3';
 var assets = [
   '/index.html',
-  '/manifest.json'
-  // 'favicon.ico',
+  '/manifest.json',
+  'favicon.ico',
+  '/logo192.png'
   // '/static/css/main.2a3f1e63.chunk.css'
 ];
 
