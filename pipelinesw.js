bc1de82c47e177cb45f06f5a09917313e3ef7784
Anchal Chauhan <anchal.c@altius.cc>
2020-01-03 13:52:41 +0530

    Updates

--- pipelinesw.js
+++ pipelinesw.js
@@ -1,4 +1,4 @@
-var cacheName = 'todo-app-v19';
+var cacheName = 'todo-app-v20';
 var assets = [
   '/pwa-react-app/index.html',
   '/pwa-react-app/manifest.json',
@@ -42,7 +42,7 @@ self.addEventListener('activate', function (event) {
   // which will get rid of static-v1
   caches.open(cacheName).then(function(cache) {
     console.log("going to call fetch ---")
-    fetch('https://anchalchouvhan.github.io/pwa-react-app/logo.png').then(function(response) {
+    fetch('pwa-react-app/logo.png').then(function(response) {
       console.log("response---"+response.statusText);
       if (!response.ok) {
         console.log("Error occured")
